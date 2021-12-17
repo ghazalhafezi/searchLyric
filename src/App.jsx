072c7823a7351685fe950c61a85e31e309eaf8e0
ghazal <hafezighazal1991@gmail.com>
2021-12-17 20:15:20 +0330

    router tag changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Search from "./components/Search";
 import Lyric from "./components/lyric/lyric";
 import {
-  BrowserRouter as Router,
-  Route, Routes
-} from 'react-router-dom'
+    BrowserRouter,
+    Routes,
+    Route
+  } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Search />}></Route>
-        <Route path="/Lyric/:musicName/:artist" element={<Lyric />} />
-      </Routes>
-    </Router>
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<App />} />
+  <Route index element={<Search />} />
+  <Route path="Lyric" element={<Lyric />} />
+  <Route path="Lyric/:musicName/:artist" render={(props) => {
+    <Lyric {...props} />
+  }} />
+    
+  
+</Routes>
+</BrowserRouter>
   )
 }
 
