c3a2d796d70127a16fe095eb3a45cfe701d9a6fc
ghazal <hafezighazal1991@gmail.com>
2021-12-18 17:23:12 +0330

    remove useless variable

--- src/components/lyric/lyric.jsx
+++ src/components/lyric/lyric.jsx
@@ -1,33 +1,27 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router";
 import "../lyric/lyric.css";
 
 export default function Lyric() {
-    
-    let { musicName, artist } = useParams();
-    const [lyric, setLyric] = useState('');
-  
-        let myMusicName = musicName
-        let myArtist = artist;
-        
-        fetch(`https://api.lyrics.ovh/v1/${myArtist}/${myMusicName}`)
-        .then(res => res.json())
-        .then(
-            (result) => {
-                result.lyrics ? 
-            setLyric(result.lyrics) :
-            setLyric(result.error);
-        },
-            (error) => {
-                let notFound = error.error;
-                setLyric(notFound);
-            }
-        );
-        return (
-            <div className = "container mt-5">
-                <h3 className = 'title'>{musicName}</h3>
-                <h2 className = 'artist'>{artist}</h2>
-                <div className = 'lyric-text'>{lyric}</div>
-            </div>
-        )
+  let { musicName, artist } = useParams();
+  const [lyric, setLyric] = useState("");
+
+  fetch(`https://api.lyrics.ovh/v1/${artist}/${musicName}`)
+    .then((res) => res.json())
+    .then(
+      (result) => {
+        result.lyrics ? setLyric(result.lyrics) : setLyric(result.error);
+      },
+      (error) => {
+        let notFound = error.error;
+        setLyric(notFound);
+      }
+    );
+  return (
+    <div className="container mt-5">
+      <h3 className="title">{musicName}</h3>
+      <h2 className="artist">{artist}</h2>
+      <div className="lyric-text">{lyric}</div>
+    </div>
+  );
 }
